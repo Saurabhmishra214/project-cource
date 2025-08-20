c6354a2fae7394e3be477acefaedc69539746121
Saurabhmishra214 <oosaurabh6@gmail.com>
2025-08-20 19:54:54 +0530

    WIP: my local changes before merge

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,12 +16,10 @@ class UserController extends Controller
 
      public function automation_course()
     {
-        // AutomationCourse model ka upyog karke saare courses database se nikaal rahe hain.
-        // `get()` method sabhi records ko ek collection ke roop mein return karta hai.
+     
         $courses = AutomationCourse::all();
 
-        // `courses` variable ko view mein pass kar rahe hain.
-        // Ab 'dashboard.automation_course' view mein `$courses` variable available hoga.
+        
         return view('dashboard.automation_course', compact('courses'));
     }
 
