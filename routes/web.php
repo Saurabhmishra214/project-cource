f25d3d1d478ee54aadc3877adc18887b3b43bd91
Saurabhmishra214 <oosaurabh6@gmail.com>
2025-08-20 13:51:01 +0530

    my changes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-
 use App\Http\Controllers\AffiliateController;
-=======
 use App\Http\Controllers\AdminController;
-
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\FrontController;
