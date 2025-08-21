79e80b5a8aa1d99aa5a1d97fd8e1b1338db26770
Gaurav Sharma <gauravsharmam24@gmail.com>
2025-08-21 18:13:54 +0530

    updated blog cards

--- resources/views/affiliate_dashboard/webinar.blade.php
+++ resources/views/affiliate_dashboard/webinar.blade.php
@@ -22,7 +22,7 @@
                         </div><!--end col-->
                     </div><!--end row-->
                     <div class="row">
-                        <div class="col-12">
+                        <div class="col-8">
                             <div class="card">
                                 <div class="card-body">
                                     <div id='calendar'></div>
